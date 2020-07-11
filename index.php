05d5a20bba9b51daed6cee2b0a1f9498848f0533
Sebastian Nunez <elias.sebastian.nunez@gmail.com>
2020-07-11 15:44:36 -0300

    agrego los viewport

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
 <html>
 	<head>
 		<meta http-equiv="Content-type" content="text/html; charset=utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">	
 		
 		<!-- Jquery -->
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
