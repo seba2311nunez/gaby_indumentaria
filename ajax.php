869ca030b05c691540912e79867024f03a18df13
Sebastian Nunez <elias.sebastian.nunez@gmail.com>
2020-07-11 21:17:43 -0300

    paso 1 con los pedidos, se muestran las cabeceras y sus items

--- ajax.php
+++ ajax.php
@@ -12,6 +12,39 @@ switch ($parametro) {
 		echo "ok";
 		
 		break;
+
+	case 'ver_pedido':
+			
+			$sql = "SELECT pt.id,modelo,color,DATE_FORMAT(pt.fechador,'%d/%m %H:%i') AS fec_pedido,
+							cliente,cantidad,precio,pagado,observacion
+						FROM pedido_item pt
+						JOIN articulo a ON pt.id_articulo=a.id  
+						WHERE pt.id_pedido=$id_pedido
+						ORDER BY cliente,pt.fechador";
+
+			$result=mysql_query($sql) or die(mysql_error()."<br>".$sql);
+
+			$json = array();
+
+			while ($row = mysql_fetch_assoc($result)) {
+				
+			    $json[] = array(
+						'id' => $row['id'],
+						'modelo' => $row['modelo'],
+						'color' => $row['color'],
+						'fec_pedido' => $row['fec_pedido'],
+						'cliente' => $row['cliente'],
+						'cantidad' => $row['cantidad'],
+						'precio' => $row['precio'],
+						'pagado' => $row['pagado'],
+						'observacion' => $row['observacion']
+						       
+			      );
+			}
+
+			echo json_encode($json);
+
+			break;	
 	
 	default:
 		# code...
--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php 
-
+include("Conectar.inc");
 
 ?>
 
@@ -51,23 +51,120 @@
 				</ul>
 			</nav>
 
-			<div class="col-md-12">
-				<div class="col-md-6">
+			<div class="row">
+				<div class="col-md-5 col-lg-5">
 					<div class="x_panel">
 						<div class="tituloDiv">
 							Listado de pedidos
 						</div>
 						<div class="row">
+							<hr>
+							<table id="tabPedido" class="table" style="margin: 10px;">
+								<thead>
+									<tr>
+										<th>Id</th>
+										<th>Estado</th>
+										<th>Fecha</th>
+										<th>Cantidad de pedidos</th>
+										<th></th>
+									</tr>
+								</thead>
+								<tbody>
+									<?php
+										$sql = "SELECT p.id,p.estado,DATE_FORMAT(p.fecha,'%d-%m-%Y') AS fecha,
+														MID(GROUP_CONCAT(DISTINCT a.modelo,'-',a.color),100) AS articulos,
+														GROUP_CONCAT(DISTINCT pt.cliente) AS clientes,
+														COUNT(*) AS q
+													FROM pedido p
+													JOIN pedido_item pt ON p.id=pt.id_pedido 
+													JOIN articulo a ON pt.id_articulo=a.id 
+													GROUP BY p.id";
+
+										$rs = mysql_query($sql) or die(mysql_error());
+
+										while($d = mysql_fetch_object($rs)){
+
+											echo "<tr>
+													<td>$d->id</td>
+													<td>$d->estado</td>
+													<td>$d->fecha</td>
+													<td style='text-align: center;'>$d->q</td>
+													<td>
+														<a class='btn btn-xs btn-info btn-verPedido' data-id_pedido='$d->id' >Ver</a>
+													</td>
+												</tr>";
+
+										}
+
+									?>
+								</tbody>
+							</table>
 							
 						</div>
 					</div>
 				</div>
-				
+				<div class="col-md-6 col-lg-6 ">
+					<div class="x_panel">
+						<div class="tituloDiv">
+							Detalle del pedido
+						</div>
+						<div class="row">
+							<table class="table table-dark" id="tabPedidoItems" style="margin: 10px;">
+								<thead>
+									<tr>
+										<th>#</th>
+										<th>Modelo</th>
+										<th>Color</th>
+										<th>Fecha</th>
+										<th>Cliente</th>
+										<th>Cantidad</th>
+										<th>Precio</th>
+										<th>Pagado</th>
+									</tr>
+								</thead>
+								<tbody></tbody>
+							</table>
+						</div>
+					</div>
+				</div>
 			</div>
+			
+
 		</div>
 		<script>
 			$(function(){
 				
+				$('#tabPedido').on('click','.btn-verPedido',function(){
+
+					$("#tabPedidoItems tbody").html(""); 
+					$("#tabPedidoItems tbody").html("<i class='fas fa-spinner fa-spin fa-2x'></i>");
+
+					console.log( $(this).data('id_pedido') )
+
+					$.getJSON('ajax.php',
+								{ parametro: "ver_pedido", id_pedido: $(this).data('id_pedido') },						       				
+								function(data){ 
+									
+									$("#tabPedidoItems tbody").html(""); 
+
+									for(var i=0; i<=data.length-1 ;i++){
+									
+										$("#tabPedidoItems tbody").append("<tr>"																							+"<td>"+(i+1)+"</td>"
+																			+"<td>"+data[i]['modelo']+"</td>"
+																			+"<td>"+data[i]['color']+"</td>"
+																			+"<td>"+data[i]['fec_pedido']+"</td>"
+																			+"<td>"+data[i]['cliente']+"</td>"
+																			+"<td>"+data[i]['cantidad']+"</td>"
+																			+"<td>"+data[i]['precio']+"</td>"
+																			+"<td>"+data[i]['pagado']+"</td>"																      				
+																		+"</tr>") ;		
+									}	
+								}//fin function data
+
+					);//fin getjson
+					
+				})
+
 			})
 		</script>
 	</body>
